b4a1ae53e2f3f3aabced67ad325fb9f77ac58d7e
JasonsTortaReviews <jason.lopezalfaro@g.austincc.edu>
2021-02-08 15:13:04 -0600

    Assingment 1

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,11 +1,27 @@
 //Write the include statement for types.h here
-
+#include "types.h"
 //Write include for capturing input from keyboard and displaying output to screen
+#include <iostream>
 
-
+using std::cout;	using std::cin; 
 
 int main() 
 {
+	cout<<"\nEnter a number you want to multiply by 5: ";
+
+	int num;
+
+	cin>>num;
+	
+	int result = mult_numbers(num);
+
+	cout<<"\nThe total value of both numbers multiplied is: "<<result<<  " (!!!!!Nice it worked!!!!)\n";
+
+	int num1 = 4;
+
+	result = mult_numbers(num1);
+
+	cout<<"\nAlso, if you didn't know, 4 * 5 is this --->>> : "<<result<<  " (!!!!!Nice it worked!!!!)\n";
 
 	return 0;
 }
