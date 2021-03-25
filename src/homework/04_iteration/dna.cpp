8709648a6c4cf80ba0855551059beb69152c08c7
JasonsTortaReviews <jason.lopezalfaro@g.austincc.edu>
2021-03-25 16:49:14 -0500

    HOMEWORK 5

--- src/homework/04_iteration/dna.cpp
+++ src/homework/04_iteration/dna.cpp
@@ -1,4 +1,7 @@
 #include "dna.h"
+#include <algorithm>
+#include <string>
+using std::string;
 /*
 Write code for function get_gc_content that accepts
 a const reference string parameter and returns a double.
@@ -6,7 +9,25 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const string& dna)
+{
+    double counter = 0;
+    double gc_content;
 
+    for(std::size_t i = 0; i < dna.size();++i)
+    {
+        if(char(dna[i]=='C') || char(dna[i]=='G')|| char(dna[i]=='c')|| char(dna[i]=='g'))
+        {
+            counter++;
+                 
+        }
+
+    }
+    double dna_strand_length = dna.length();
+    gc_content = 100*(counter/dna_strand_length);
+
+    return gc_content;
+}
 
 
 
@@ -14,6 +35,23 @@ Return quotient.
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
+string get_reverse_string(string dna)
+{
+  int len = dna.length();
+  int n=len-1;
+  for(int i=0;i<(len/2);i++){
+
+    //Using temp to store the char value at index i so 
+    //you can swap it in later for char value at index n
+    char temp = dna[i];
+    dna[i] = dna[n];
+    dna[n] = temp;
+    n = n-1;
+
+  }
+    return dna;
+}
+
 
 
 
@@ -27,4 +65,31 @@ b. iterate local string variable and
 c. return string
 
 */
-
+string get_dna_complement(string dna)
+{
+    string reverse_dna;
+    
+    reverse_dna  = get_reverse_string(dna);
+    
+    for(std::size_t i = 0; i <= reverse_dna.size();++i)
+    {
+        switch(reverse_dna[i])
+        {
+            case 'C':
+                reverse_dna[i]='G';
+                break;
+            case 'G':
+                reverse_dna[i]='C';
+                break;
+            case 'A':
+                reverse_dna[i]='T';
+                break;
+            case 'T':
+                reverse_dna[i]='A';
+                break;
+            
+            
+        }
+    }
+    return reverse_dna;
+}
--- src/homework/04_iteration/main.cpp
+++ src/homework/04_iteration/main.cpp
@@ -1,7 +1,10 @@
 //write include statements
+#include "dna.h"
+#include <iostream>
+#include <string>
 
 //write using statements
-
+using std::string; using std::endl; using std::cout; using std::cin; 
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +15,56 @@ user enters a y or Y.
 */
 int main() 
 {
+	 string continue_;
+     string dna;
+    do
+    {
+        bool end_loop = false; 
+    
+        while(end_loop == false)
+        {
+            int input;
+			
+			cout<<endl<<"For GC content in the DNA Strand PRESS: 1"<<endl;
+			cout<<"For the DNA Strands Complement PRESS: 2"<<endl;
+            cout<<endl<<"Select (1 or 2): ";cin>>input;cout<<endl;
+
+            if(input == 1)
+            {
+
+				cout<<"You chose GC Content Calculator!"<<endl;
+
+				cout<<endl<<"Give me the DNA strand you would like to test: ";cin>>dna;cout<<endl;
+
+				int gc_content = get_gc_content(dna);
+
+				cout<<"The GC content in "<<dna<<" is: "<<gc_content<<"%"<<endl;
+
+                end_loop = true;
+
+                
+            }
+			else if( input == 2)
+			{
+
+                cout<<"You chose DNA Complement Calculator"<<endl;
+
+                cout<<endl<<"Give me the DNA strand you would like to find the Complement of: ";cin>>dna;cout<<endl;
+
+                string complement_dna = get_dna_complement(dna);
+
+                cout<<"The DNA of Complement of "<<dna<<" is: "<<complement_dna<<endl;
+                end_loop = true;
+			}
+            else
+            {
+                cout<<"Youre Weird Try Again"<<endl;
+                end_loop = false;
+            }
+        }     
+        
+        cout<<"Would you like to CONTINUE ( Y or N ): ";cin>>continue_;cout<<endl;
+    }while( continue_ == "y"|| continue_ == "Y");
+
 	return 0;
 }
